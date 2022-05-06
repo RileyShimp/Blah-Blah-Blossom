7645d064d06574d3e637b3329cb5bf1dac15f580
Riley Shimp <rilryshimp@gmail.com>
2022-05-06 14:07:01 -0700

    adjust navbar and img size

--- client/src/components/NewWord.js
+++ client/src/components/NewWord.js
@@ -2,13 +2,15 @@ import React from "react";
 import { useState } from "react";
 import Axios from "axios";
 import Button from '@mui/material/Button';
+import { cardActionAreaClasses } from "@mui/material";
 
 function NewWord() {
-  const [displayImage, setDisplayImage] = useState([]);
+  const [displayImage, setDisplayImage] = useState({img:"car", word:"Car"});
 
   const newWord = () => {
     Axios.get("http://localhost:3001/words").then((response) => {
       setDisplayImage(response.data);
+      console.log(response.data);
     });
   };
 
@@ -16,7 +18,7 @@ function NewWord() {
     <div className="NewWordContainer">
       <Button variant="contained" onClick={newWord}>New Word</Button>
     <h1>{displayImage.word}</h1>
-    <img alt="word" src={(`../assets/img/${displayImage.img}.JPG`)} />
+    <img alt="word" src={require(`../assets/img/${displayImage.img}.JPG`)} />
       {/* {displayImage.map((val, key) => {
         return (
           <div className="wordDisplayed">
--- client/src/components/SimpleBottomNavigation.js
+++ client/src/components/SimpleBottomNavigation.js
@@ -5,22 +5,27 @@ import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import HomeIcon from '@mui/icons-material/Home';
 import ShuffleIcon from '@mui/icons-material/Shuffle';
+
+
 export default function SimpleBottomNavigation() {
+
   const [value, setValue] = React.useState(0);
 
   return (
-    <Box sx={{ width: 500 }}>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-        <BottomNavigationAction label="Flash Cards" icon={<ShuffleIcon />} />
-        <BottomNavigationAction label="Play" icon={<SportsEsportsIcon />} />
-      </BottomNavigation>
-    </Box>
+    <div class='navBar'>
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+          <BottomNavigationAction label="Flash Cards" icon={<ShuffleIcon />} />
+          <BottomNavigationAction label="Play" icon={<SportsEsportsIcon />} />
+        </BottomNavigation>
+      </Box>
+    </div>
   );
 }
