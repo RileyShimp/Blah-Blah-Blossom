a68425f75ff84fdf9f888fda97705e05f650760b
Riley Shimp <rilryshimp@gmail.com>
2022-05-13 11:16:32 -0700

    Adjust image list to only show 4 images

--- client/src/components/Play.js
+++ client/src/components/Play.js
@@ -7,7 +7,38 @@ import ImageListItem from '@mui/material/ImageListItem';
 
 export default function Play() {
 
-  const [words, setWords] = useState([{img:"car", word:"Car"}]);
+  const [words, setWords] = useState([
+    {
+      "id": 4,
+      "img": "dog",
+      "audio": null,
+      "word": "Dog"
+    },
+    {
+      "id": 3,
+      "img": "car",
+      "audio": null,
+      "word": "Car"
+    },
+    {
+      "id": 2,
+      "img": "banana",
+      "audio": null,
+      "word": "Banana"
+    },
+    {
+      "id": 1,
+      "img": "apple",
+      "audio": null,
+      "word": "Apple"
+    },
+    {
+      "id": 5,
+      "img": "bottle",
+      "audio": null,
+      "word": "Bottle"
+    }
+]);
 
   const newGame = () => {
     Axios.get("http://localhost:3001/play").then((response) => {
@@ -15,37 +46,40 @@ export default function Play() {
       console.log(response.data);
     });
   };
-
   
   return (
-//     <div>
-//       <button onClick={newGame}>Play</button>
-//       <h1>Game Loading...</h1>
-//             {words.map((val, key) => {
-//         return (
-//           <div className="wordDisplayed">
-//             <h2> {val.word} </h2>
-//             <img alt="word" src={require(`../assets/img/${val.img}.JPG`)} />
-//           </div>
-//         );
-//       })}
-//     </div>
-//   )
-// }
     <div>
-    <button onClick={newGame}>Play</button>
-    <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
-      {words.map((val,key) => (
-        <ImageListItem key={val.word}>
-          <img
-            src={require(`../assets/img/${val.img}.JPG?w=164&h=164&fit=crop&auto=format`)}
-            srcSet={require(`../assets/img/${val.img}.JPG?w=164&h=164&fit=crop&auto=format`)}
-            alt={val.word}
-            loading="lazy"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+      <button onClick={newGame}>Play</button>
+      <ImageList sx={{ width: 400, height: 365 }} cols={2} rowHeight={164}>
+          <ImageListItem>
+            <img
+              src={require(`../assets/img/${words[1].img}.JPG?w=164&h=164&fit=crop&auto=format`)}
+              alt={words[1].word}
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src={require(`../assets/img/${words[2].img}.JPG?w=164&h=164&fit=crop&auto=format`)}
+              alt={words[2].word}
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src={require(`../assets/img/${words[3].img}.JPG?w=164&h=164&fit=crop&auto=format`)}
+              alt={words[3].word}
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src={require(`../assets/img/${words[4].img}.JPG?w=164&h=164&fit=crop&auto=format`)}
+              alt={words[4].word}
+              loading="lazy"
+            />
+          </ImageListItem>
+      </ImageList>
     </div>
   );
 }
