7745cf10c2eb4c7610472cc0bfd030b6e50c45da
Riley Shimp <rilryshimp@gmail.com>
2022-05-28 11:17:37 -0700

    Move db info to config

--- server/index.js
+++ server/index.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
 const cors = require("cors");
+const db = require("./config/db");
 
 app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
-
-const db = mysql.createConnection({
-  user: "b3d77a579fe705",
-  host: "us-cdbr-east-05.cleardb.net",
-  password: "d4b21317",
-  database: "heroku_2058cf205184fa2",
-});
-
-// mysql://b3d77a579fe705:d4b21317@us-cdbr-east-05.cleardb.net/heroku_2058cf205184fa2?reconnect=true
 
 app.get("/words", (req, res) => {
   db.query("SELECT * FROM words ORDER BY RAND()", (err, result) => {
